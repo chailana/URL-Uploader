9c580354f22c803e866f75974b4158532c6a7e7b
chailana <chailana35217793@gmail.com>
2024-09-07 22:39:56 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,8 +5,7 @@ wget -O /app/Uploader/aria2/dht.dat https://github.com/P3TERX/aria2.conf/raw/mas
 wget -O /app/Uploader/aria2/dht6.dat https://github.com/P3TERX/aria2.conf/raw/master/dht6.dat
 TRACKER=`curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt -: https://ngosang.github.io/trackerslist/trackers_all_http.txt -: https://newtrackon.com/api/all -: https://raw.githubusercontent.com/DeSireFire/animeTrackerList/master/AT_all.txt -: https://torrends.to/torrent-tracker-list/?download=latest | awk '$1' | tr '\n' ',' | cat`
 ran=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
-###### Done Addding Files ######
-
+###### Done Adding Files ######
 
 if [[ -n $RCLONE_CONFIG_URL ]]; then
   echo "Rclone config detected 📁📁"
@@ -14,12 +13,12 @@ if [[ -n $RCLONE_CONFIG_URL ]]; then
 fi
 
 if [[ -n $CONFIG_ENV_URL ]]; then
-  echo " Found config.env File 📁📁 "
-	wget -q $CONFIG_ENV_URL -O /app/config.env
+  echo "Found config.env File 📁📁"
+  wget -q $CONFIG_ENV_URL -O /app/config.env
 fi
-
-
 
 echo "Starting Your Bot... ✨✨"
 python3 -m Uploader
 
+# Start the app
+python3 app.py
